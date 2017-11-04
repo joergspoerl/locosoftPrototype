9ce9ec48355738b2372305293c16238cc1d33401
Joerg Spoerl <joerg.spoerl@gmx.de>
2017-11-04 12:10:37 +0100

    edit pusblish-uberspace

---
+++ publish-uberspace.sh
@@ -0,0 +1,9 @@
+# replicate www folder 
+ionic build browser
+rsync -ruv --stats --progress www/ jrg@deneb.uberspace.de:/home/jrg/html/locosoftPrototype/
+
+# create apk & deploy
+git push
+ionic cordova build android
+rsync -ruv --stats --progress platforms/android/build/outputs/apk/android-armv7-debug.apk jrg@deneb.uberspace.de:/home/jrg/html/locosoftPrototype/android-armv7-debug.apk
+~/scripts/sendMail-apk-ready.sh "http://jrg.deneb.uberspace.de/locosoftPrototype/android-armv7-debug.apk"
--- rsync-uberspace.sh
+++
@@ -1,9 +0,0 @@
-# replicate www folder 
-ionic build browser
-rsync -ruv --stats --progress www/ jrg@deneb.uberspace.de:/home/jrg/html/locosoftPrototype/
-
-# create apk & deploy
-git push
-ionic cordova build android
-rsync -ruv --stats --progress platforms/android/build/outputs/apk/android-armv7-debug.apk jrg@deneb.uberspace.de:/home/jrg/html/mw/android-armv7-debug.apk
-~/scripts/sendMail-apk-ready.sh
