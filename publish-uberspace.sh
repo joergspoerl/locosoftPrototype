754c10da35108611ae5429d66aca73316e6d6ff4
Joerg Spoerl <joerg.spoerl@gmx.de>
2017-11-06 17:06:49 +0100

    fix release file name

--- publish-uberspace.sh
+++ publish-uberspace.sh
@@ -5,5 +5,7 @@ rsync -ruv --stats --progress www/ jrg@deneb.uberspace.de:/home/jrg/html/locosof
 # create apk & deploy
 git push
 ionic cordova build android
+ionic cordova build android --prod --release
 rsync -ruv --stats --progress platforms/android/build/outputs/apk/android-armv7-debug.apk jrg@deneb.uberspace.de:/home/jrg/html/locosoftPrototype/android-armv7-debug.apk
-~/scripts/sendMail-apk-ready.sh "http://jrg.deneb.uberspace.de/locosoftPrototype/android-armv7-debug.apk"
+rsync -ruv --stats --progress platforms/android/build/outputs/apk/android-armv7-release-unsigned.apk jrg@deneb.uberspace.de:/home/jrg/html/locosoftPrototype/android-armv7-release-unsigned.apk
+~/scripts/sendMail-apk-ready.sh "http://jrg.deneb.uberspace.de/locosoftPrototype/android-armv7-debug.apk" "http://jrg.deneb.uberspace.de/locosoftPrototype/android-armv7-release-unsigned.apk"
