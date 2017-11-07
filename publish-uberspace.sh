0752d7e33263e4c13961a9c68c8cd3943aab21b4
Joerg Spoerl <joerg.spoerl@gmx.de>
2017-11-07 07:06:39 +0100

    publish script add Browser URL

--- publish-uberspace.sh
+++ publish-uberspace.sh
@@ -5,7 +5,8 @@ rsync -ruv --stats --progress www/ jrg@deneb.uberspace.de:/home/jrg/html/locosof
 # create apk & deploy
 git push
 ionic cordova build android
-ionic cordova build android --prod --release
+#ionic cordova build android --prod --release
 rsync -ruv --stats --progress platforms/android/build/outputs/apk/android-armv7-debug.apk jrg@deneb.uberspace.de:/home/jrg/html/locosoftPrototype/android-armv7-debug.apk
-rsync -ruv --stats --progress platforms/android/build/outputs/apk/android-armv7-release-unsigned.apk jrg@deneb.uberspace.de:/home/jrg/html/locosoftPrototype/android-armv7-release-unsigned.apk
-~/scripts/sendMail-apk-ready.sh "http://jrg.deneb.uberspace.de/locosoftPrototype/android-armv7-debug.apk" "http://jrg.deneb.uberspace.de/locosoftPrototype/android-armv7-release-unsigned.apk"
+#rsync -ruv --stats --progress platforms/android/build/outputs/apk/android-armv7-release-unsigned.apk jrg@deneb.uberspace.de:/home/jrg/html/locosoftPrototype/android-armv7-release-unsigned.apk
+#~/scripts/sendMail-apk-ready.sh "https://jrg.deneb.uberspace.de/locosoftPrototype/android-armv7-debug.apk" "https://jrg.deneb.uberspace.de/locosoftPrototype/android-armv7-release-unsigned.apk"  "https://jrg.deneb.uberspace.de/locosoftPrototype/"
+~/scripts/sendMail-apk-ready.sh "https://jrg.deneb.uberspace.de/locosoftPrototype/android-armv7-debug.apk" "not available" "https://jrg.deneb.uberspace.de/locosoftPrototype/"
